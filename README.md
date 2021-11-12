f2ee613f99e751f079119344cebf83f574c4a239
Jerry Lu <jclu2gm@gmail.com>
2021-11-11 17:37:29 -0800

    Finished expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+Jerry Lu
+1. I would put the code in a Github action that runs whenever code is pushed so that there would be an immediate response for the developers pushing the code.  
+2. No  
+3. No, writing and receiving messages is an example of individual components intereacting with each other that unit testing can not cover.  
+4. Yes, testing the max message length can be done on a small scale without many moving parts. The max message length does not involve many individual componenets interacting with each other.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,83 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Part 2
+test("valid phone number 1", () => {
+    expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+});
+
+test("valid phone number 2", () => {
+    expect(functions.isPhoneNumber("111-111-1111")).toBe(true);
+});
+
+test("invalid phone number 1", () => {
+    expect(functions.isPhoneNumber("12345-678-90")).toBe(false);
+});
+
+test("invalid phone number 2", () => {
+    expect(functions.isPhoneNumber("1111111111")).toBe(false);
+});
+
+test("valid email 1", () => {
+    expect(functions.isEmail("Richter@Belmont.com")).toBe(true);
+});
+
+test("valid email 2", () => {
+    expect(functions.isEmail("SomaCruz@DraculasCastle.gov")).toBe(true);
+});
+
+test("invalid email 1", () => {
+    expect(functions.isEmail("@WhatIsAMan.com")).toBe(false);
+});
+
+test("invalid email 2", () => {
+    expect(functions.isEmail("YouDontBelongInThisWorld@Dracula")).toBe(false);
+});
+
+test("strong password 1", () => {
+    expect(functions.isStrongPassword("passwordAdmin")).toBe(true);
+});
+
+test("strong password 2", () => {
+    expect(functions.isStrongPassword("password123")).toBe(true);
+});
+
+test("weak password 1", () => {
+    expect(functions.isStrongPassword("1000blades")).toBe(false);
+});
+
+test("weak password 2", () => {
+    expect(functions.isStrongPassword("ItWasNotByMyHandThatIAmOnceAgain")).toBe(false);
+});
+
+test("valid date 1", () => {
+    expect(functions.isDate("05/06/2003")).toBe(true);
+});
+
+test("valid date 2", () => {
+    expect(functions.isDate("04/20/1990")).toBe(true);
+});
+
+test("invalid date 1", () => {
+    expect(functions.isDate("11/14/06")).toBe(false);
+});
+
+test("invalid date 2", () => {
+    expect(functions.isDate("4/5/94")).toBe(false);
+});
+
+test("valid hex color 1", () => {
+    expect(functions.isHexColor("FFFFFF")).toBe(true);
+});
+
+test("valid hex color 2", () => {
+    expect(functions.isHexColor("000000")).toBe(true);
+});
+
+test("invalid hex color 1", () => {
+    expect(functions.isHexColor("FFFFF")).toBe(false);
+});
+
+test("invalid hex color 2", () => {
+    expect(functions.isHexColor("FF")).toBe(false);
+});
